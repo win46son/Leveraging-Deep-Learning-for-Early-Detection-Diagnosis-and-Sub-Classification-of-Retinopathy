d65d0bb52b8a3f96f31f46753942b16b818b64b1
win46son <165797074+win46son@users.noreply.github.com>
2025-06-04 11:49:37 +0800

    Update SubClass_CSV.py

--- SubClass_CSV.py
+++ SubClass_CSV.py
@@ -1,8 +1,3 @@
-## 提取softmax值，出来form csv
-## csv有了，就正常build xgboost model
-## if condition - 5个model
-## 先过efficientnet， net forward判断结果，然后选sub-class model，同时提取他的softmax值
-
 import torch
 from PIL import Image
 from DataPreparation_1 import train_transform
@@ -60,12 +55,4 @@ for label in label_names:
 
         df_write = pd.DataFrame(results)
         df_write.to_csv(f'DR/softmax_results/{loc}/{label}.csv', index=False)
-        print(f"结果已保存到 {loc}-{label} csv file，共处理了 {len(results)} 张图片")
-
-
-# df = pd.read_csv(f'DR\\class_wise_subclasses\\Mild\\Mild_cluster_mapping.csv')
-# df_name_list = df['filename'].tolist()
-# print(df_name_list[:15])
-# if '0dce95217626' in df_name_list:
-#     print(df_name_list.index('0dce95217626'))
-#     print(df['cluster'][df_name_list.index('0dce95217626')])
+        print(f"Results have been saved to {loc}-{label} csv file，total handling {len(results)} images")
